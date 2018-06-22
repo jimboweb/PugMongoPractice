b1aa999fdc060415895a5052a272421b71db2469
jimboweb <jimboweb@gmail.com>
2018-06-22 09:48:08 -0400

    api post and db created

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoClient = require('mongodb').MongoClient;
+mongoClient.connect('mongodb://localhost:27017/names',function(err,db){
+    if(err) throw err;
+    console.log("names database created");
+})
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
 
 const app = express();
 
@@ -22,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET api calls */
 router.get('/', function(req, res, next) {
-
+    res.send('api listening');
+    next();
 });
+
+router.post('/',function (req,res,next) {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    res.send("Your name is " + firstName + " " + lastName);
+    next();
+})
 
 module.exports = router;
